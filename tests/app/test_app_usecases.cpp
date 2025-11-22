434d58ffc99bdc419aaf849efe663fb8562b4785
axumie <vikadvnilova@gmail.com>
2025-11-22 16:19:55 +0200

    Fix tests for ConfigLoadUseCase

--- tests/app/test_app_usecases.cpp
+++ tests/app/test_app_usecases.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <string>
+#include <iostream>
 
 #include "app/usecases/NewGameUseCase.hpp"
 #include "app/usecases/SaveGameUseCase.hpp"
@@ -9,6 +10,7 @@
 #include "domain/rules/GameRules.hpp"
 #include "domain/core/GameState.hpp"
 #include "app/usecases/ConfigLoadUseCase.hpp"
+#include "domain/rules/GameRules.hpp"
 
 struct DummyRng : Domain::Core::IRng
 {
@@ -22,18 +24,60 @@ struct DummyRepo : Application::Persistence::ISaveGameRepo
     bool load(Domain::Core::GameState &, const std::string &) override { return false; }
 };
 
-bool configload()
+[[nodiscard]] bool test_config_load_returns_default_rules()
 {
-    auto rules = Application::Usecases::ConfigLoadUseCase::load_default();
-    return (rules.map_w == Domain::Rules::DEFAULT_RULES.map_w) &&
-           (rules.map_h == Domain::Rules::DEFAULT_RULES.map_h) &&
-           (rules.enemy_count == Domain::Rules::DEFAULT_RULES.enemy_count) &&
-           (rules.potion_heal_max == Domain::Rules::DEFAULT_RULES.potion_heal_max);
+    const auto expected = Domain::Rules::DEFAULT_RULES;
+    const auto rules = Application::Usecases::ConfigLoadUseCase::load_default();
+
+    bool ok = true;
+
+    const auto check_equal = [&](auto actual, auto exp, std::string_view name)
+    {
+        if (actual != exp)
+        {
+            std::cerr << "ConfigLoadUseCase::load_default(): '" << name
+                      << "' mismatch: actual=" << actual << ", expected=" << exp << '\n';
+            ok = false;
+        }
+    };
+
+    check_equal(rules.map_w, expected.map_w, "map_w");
+    check_equal(rules.map_h, expected.map_h, "map_h");
+    check_equal(rules.enemy_count, expected.enemy_count, "enemy_count");
+    check_equal(rules.potion_heal_max, expected.potion_heal_max, "potion_heal_max");
+
+    return ok;
+}
+
+[[nodiscard]] bool test_new_game_executes()
+{
+    Domain::Core::GameState st{};
+    Domain::Rules::GameRules rules{};
+    DummyRng rng{};
+
+    Application::Usecases::NewGameUseCase::execute(st, rules, rng);
+    // TODO: add real assertions on `st` and `rules`
+    return true;
+}
+
+[[nodiscard]] bool test_save_and_load_do_not_crash()
+{
+    Domain::Core::GameState st{};
+    DummyRepo repo{};
+
+    Application::Usecases::SaveGameUseCase::save(repo, st, "x.sav");
+    Application::Usecases::LoadGameUseCase::load(repo, st, "x.sav");
+
+    // TODO: add expectations when DummyRepo is improved
+    return true;
 }
 
 int main()
 {
-    if (!configload())
-        return EXIT_FAILURE;
-    return EXIT_SUCCESS;
+    bool alright = true;
+    alright &= test_config_load_returns_default_rules();
+    alright &= test_new_game_executes();
+    alright &= test_save_and_load_do_not_crash();
+
+    return alright ? EXIT_SUCCESS : EXIT_FAILURE;
 }
