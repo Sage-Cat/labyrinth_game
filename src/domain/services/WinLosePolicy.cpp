7e1fc7e6636aa5fc549138462c1c7fc02486d486
funnylime1 <perhajlomaksim@gmail.com>
2025-11-22 20:49:54 +0000

    feature/7-win/lose-policy

--- src/domain/services/WinLosePolicy.cpp
+++ src/domain/services/WinLosePolicy.cpp
@@ -1,10 +1,22 @@
 #include "domain/services/WinLosePolicy.hpp"
 #include "domain/core/GameState.hpp"
+#include "domain/entities/actors/Player.hpp"
 
 namespace Domain::Services
 {
-    void WinLosePolicy::evaluate(Domain::Core::GameState &)
+    void WinLosePolicy::evaluate(Domain::Core::GameState &state)
     {
-        // TODO: evaluate win/lose condition base on game state (exit or find a key)
+        for (auto &actor : state.actors)
+        {
+            auto* player = dynamic_cast<Domain::Entities::Player*>(actor.get());
+            if (player && checkPlayerHp(*player))
+            {
+      state.defeat = true;
+       return;
+    }
+    }
+    }
+    bool checkPlayerHp( Domain::Entities::Player &player){
+    return player.stats.hp <= 0;
     }
 }
